8173d2ac4a48a05793ace10f84ea91e462d9b9ef
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-21 12:57:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  (base_hash.keys.count).to(eq(1)
+ 
 end
 
 def monopoly_with_fourth_tier
@@ -33,5 +33,5 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  (base_hash.keys.count).to(eq(1)
+ 
 end
